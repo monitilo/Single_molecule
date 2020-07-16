6cca7e5ee3a795682eae86f3283597e6631af503
CHIARELLI German <german.chiarelli@unifr.ch>
2020-07-16 16:59:12 +0200

    Change the dock tabs color!!!

--- SMAnalyzer_NP edition 2020.py
+++ SMAnalyzer_NP edition 2020.py
@@ -86,10 +86,58 @@ from scipy import optimize
 
 import os
 from pyqtgraph.dockarea import Dock, DockArea
-
-
+from pyqtgraph.dockarea.Dock import DockLabel
 
 #import time as time
+
+# %% Change the colors of the Dock tabs
+def updateStylePatched(self):
+    r = '3px'
+    if self.dim:
+        fg = '#b0b0b0'
+        bg = '#94f5bb'
+        border = '#94f5bb'
+#        border = '#7cf3ac'
+    else:
+        fg = '#fff'
+        bg = '#10b151'
+        border = '#10b151'
+
+    if self.orientation == 'vertical':
+        self.vStyle = """DockLabel {
+            background-color : %s;
+            color : %s;
+            border-top-right-radius: 0px;
+            border-top-left-radius: %s;
+            border-bottom-right-radius: 0px;
+            border-bottom-left-radius: %s;
+            border-width: 0px;
+            border-right: 2px solid %s;
+            padding-top: 3px;
+            padding-bottom: 3px;
+            font-size: 18px;
+        }""" % (bg, fg, r, r, border)
+        self.setStyleSheet(self.vStyle)
+    else:
+        self.hStyle = """DockLabel {
+            background-color : %s;
+            color : %s;
+            border-top-right-radius: %s;
+            border-top-left-radius: %s;
+            border-bottom-right-radius: 0px;
+            border-bottom-left-radius: 0px;
+            border-width: 0px;
+            border-bottom: 2px solid %s;
+            padding-left: 13px;
+            padding-right: 13px;
+            font-size: 18px
+        }""" % (bg, fg, r, r, border)
+        self.setStyleSheet(self.hStyle)
+
+
+DockLabel.updateStyle = updateStylePatched
+
+
 
 class smAnalyzer(pg.Qt.QtGui.QMainWindow):
 
